0e924280e0e4525114c0ab5a49c2c6fcb4707e17
abdullahsheikh7177-lang <abdullahsheikh7177@gmail.com>
2026-08-05 21:23:01 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,24 @@
-// Offline Service Worker
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
-
-const CACHE = "pwabuilder-page-v2";
+const CACHE = "pwabuilder-page-v3";
 
 const filesToCache = [
-"./",
-"./index.html",
-"./notice.html",
-"./manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-self.addEventListener("message", (event) => {
-if (event.data && event.data.type === "SKIP_WAITING") {
-self.skipWaiting();
-}
-});
-
 self.addEventListener("install", (event) => {
-event.waitUntil(
-caches.open(CACHE).then((cache) => {
-return cache.addAll(filesToCache);
-})
-);
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-
-event.respondWith(
-
-caches.match(event.request).then((response) => {
-
-return response || fetch(event.request);
-
-}).catch(() => {
-
-if (event.request.mode === "navigate") {
-return caches.match("./index.html");
-}
-
-})
-
-);
-
+  event.respondWith(fetch(event.request));
 });
